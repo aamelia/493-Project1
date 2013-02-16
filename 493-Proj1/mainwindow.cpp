5a68372dadc2b00514f7c32fe238db482d1b4fae
aamelia <aamelia@umich.edu>
2013-02-15 22:56:25 -0500

    Playing around with Label and List View

--- 493-Proj1/mainwindow.cpp
+++ 493-Proj1/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    statusBar()->showMessage("QSimulate has started");
 }
 
 MainWindow::~MainWindow()
